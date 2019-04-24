3b9a21d767f976223df06dc9fd6ac3da4c2fb81b
Steven Gibson <sgibson@ospreysoftware.com>
2019-04-24 11:22:36 -0400

    added coverage and modulemapped UI

--- wwwroot/test/components/reactcomponent.test.js
+++ wwwroot/test/components/reactcomponent.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import Counter from '../../Source/reactcomponent';
+import Counter from 'ui/Source/reactcomponent';
 
 describe('Test Counter component', () => {
      it('renders the counter component successfully', () => {
